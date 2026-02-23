ceeb9135427beb868c4ec023dca8dea1b23a8dba
Rubelhasan29 <rubelhasanmd29@gmail.com>
2026-02-23 16:28:18 +0600

    in the created div in the filtered section div some class name set innerHTML of it

--- script.js
+++ script.js
@@ -80,7 +80,42 @@ function renderInterview() {
         console.log(interview);
 
         let div = document.createElement('div')
-        
+        div.className = 'card flex justify-between p-8 shadow-md bg-white border border-gray-200 rounded-lg'
+        div.innerHTML = `
+                        <div class="card left space-y-6">
+
+                    <div class="left-top space-y-4">
+                        <p class="workPosition text-2xl font-bold opacity-80">Mobile First Corp</p>
+                        <p class="jobPosition opacity-60">React Native Developer</p>
+                        <p class="jobDetails opacity-60">Remote • Full-time • $130,000 - $175,000</p>
+                    </div>
+
+
+                    <div>
+                        <p id="status" class="bg-blue-100 opacity-80 inline px-[14px] py-3 font-semibold text-black">NOT
+                            APPLIED</p>
+                    </div>
+                    <div>
+                        <p class="jobDescription opacity-80 ">Build cross-platform mobile applications using React Native. Work on
+                            products
+                            used by millions of
+                            users worldwide.</p>
+                    </div>
+
+                    <div class="flex flex-col sm:flex-row gap-6">
+                        <button
+                            class="interview-btn px-5 py-2 text-green-500 border-[2px] border-green-500 text-lg font-semibold rounded-md">INTERVIEW</button>
+                        <button
+                            class="rejected-btn px-6 py-2 text-red-500 border-[2px] border-red-500 text-lg font-semibold rounded-md">REJECTED</button>
+                    </div>
+                </div>
+
+                <div class="card-right text-center">
+                    <button onclick="deleteApplication('delete-btn')"
+                        class="border-2 border-gray-200 p-[10px] px-[11px] rounded-full text-[13px]" id="delete-btn"><i
+                            class="fa-solid fa-trash"></i></button>
+                </div>
+        `
     }
 }
 
