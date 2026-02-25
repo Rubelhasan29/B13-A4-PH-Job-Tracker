4e58bcfca8993009f863e9282a3e41bd74fc0732
Rubelhasan29 <rubelhasanmd29@gmail.com>
2026-02-25 15:22:31 +0600

    make the function to change the All buttons and tabs and the styles of the buttons

--- script.js
+++ script.js
@@ -1,287 +1,38 @@
-let interviewCount = [];
-let rejectedCount = [];
-let currentStatus = 'all'
+let currentTab = "all";
+const tabActive = ['bg-blue-400', 'border-gray-300', 'shadow-sm', 'py-3', 'px-11', 'rounded-md', 'text-xl']
+const tabInactive = ['bg-white', 'border', 'border-gray-300', 'shadow-sm', 'py-3', 'px-11', 'rounded-md', 'text-xl']
 
+const allContainer = document.getElementById('all-container')
+const interviewContainer = document.getElementById('interview-container')
+const rejectedContainer = document.getElementById('rejected-container')
 
-let allFilterBtn = document.getElementById('all-filter-btn')
-let interviewFilterBtn = document.getElementById('interview-filter-btn')
-let rejectedFilterBtn = document.getElementById('rejected-filter-btn')
 
+function changeTab(tab) {
 
+    const tabs = ['all', 'interview', 'rejected'];
 
-let total = document.getElementById('total-count');
-let interview = document.getElementById('interview-count')
-let rejected = document.getElementById('rejected-count')
-
-
-
-let allCardSection = document.getElementById('allCard');
-
-
-
-let mainContainer = document.querySelector('main')
-let filteredSection = document.getElementById('filteredSection')
-
-
-
-function calculate() {
-
-
-    interview.innerText = interviewCount.length;
-    rejected.innerText = rejectedCount.length;
-}
-calculate();
-
-
-function filtering(id) {
-    allFilterBtn.classList.remove('bg-blue-500', 'bg-white', 'text-white')
-    interviewFilterBtn.classList.remove('bg-blue-500', 'bg-white', 'text-white')
-    rejectedFilterBtn.classList.remove('bg-blue-500', 'bg-white', 'text-white')
-
-
-    const selected = document.getElementById(id);
-    selected.classList.add('bg-blue-500', 'text-white');
-
-    currentStatus = id
-
-    if (id == 'interview-filter-btn') {
-        allCardSection.classList.add('hidden');
-        filteredSection.classList.remove('hidden');
-        renderInterview()
-    } else if (id == 'all-filter-btn') {
-        allCardSection.classList.remove('hidden');
-        filteredSection.classList.add('hidden');
-        checkEmpty(allCardSection);
-
-    } else if (id == 'rejected-filter-btn') {
-        allCardSection.classList.add('hidden');
-        filteredSection.classList.remove('hidden');
-        renderRejected();
-    }
-}
-
-
-
-mainContainer.addEventListener('click', function (event) {
-
-    if (event.target.classList.contains('interview-btn')) {
-        const parentNode = event.target.parentNode.parentNode;
-
-        const workPosition = parentNode.querySelector('.workPosition').innerText
-        const jobPosition = parentNode.querySelector('.jobPosition').innerText
-        const jobDetails = parentNode.querySelector('.jobDetails').innerText
-        const status = parentNode.querySelector('.status').innerText
-        const jobDescription = parentNode.querySelector('.jobDescription').innerText
-
-
-
-        parentNode.querySelector('.status').innerText = "INTERVIEW";
-
-        const cardInfo = {
-            workPosition,
-            jobPosition,
-            jobDetails,
-            status: 'INTERVIEW',
-            jobDescription
-        }
-
-        const workPositionExist = interviewCount.find(item => item.workPosition == cardInfo.workPosition);
-
-        if (!workPositionExist) {
-            interviewCount.push(cardInfo);
-        }
-
-        rejectedCount = rejectedCount.filter(item => item.workPosition != cardInfo.workPosition);
-
-        updateAllSectionStatus(workPosition, 'INTERVIEW');
-
-        if (currentStatus == 'rejected-filter-btn') {
-            renderRejected()
-        }
-
-        calculate();
-    }
-
-
-
-
-
-    else if (event.target.classList.contains('rejected-btn')) {
-        const parentNode = event.target.parentNode.parentNode;
-
-        console.log(parentNode);
-
-        const workPosition = parentNode.querySelector('.workPosition').innerText
-        const jobPosition = parentNode.querySelector('.jobPosition').innerText
-        const jobDetails = parentNode.querySelector('.jobDetails').innerText
-        const status = parentNode.querySelector('.status').innerText
-        const jobDescription = parentNode.querySelector('.jobDescription').innerText
-
-        parentNode.querySelector('.status').innerText = 'REJECTED';
-
-
-
-        const cardInfo = {
-            workPosition,
-            jobPosition,
-            jobDetails,
-            status: 'REJECTED',
-            jobDescription
-        }
-        console.log(status);
-
-        const workPositionExist = rejectedCount.find(item => item.workPosition == cardInfo.workPosition);
-
-        if (!workPositionExist) {
-            rejectedCount.push(cardInfo);
-        }
-
-        interviewCount = interviewCount.filter(item => item.workPosition != cardInfo.workPosition);
-
-        updateAllSectionStatus(workPosition, 'REJECTED');
-
-
-        if (currentStatus == 'interview-filter-btn') {
-            renderInterview()
-        }
-        calculate();
-
-    }
-
-})
-
-function renderInterview() {
-    filteredSection.innerHTML = ''
-
-    for (let interview of interviewCount) {
-
-        let div = document.createElement('div')
-        div.className = 'card flex justify-between p-8 shadow-md bg-white border border-gray-200 rounded-lg'
-        div.innerHTML = `
-                        <div class="card left space-y-6">
-
-                    <div class="left-top space-y-4">
-                        <p class="workPosition text-2xl font-bold opacity-80">${interview.workPosition}</p>
-                        <p class="jobPosition opacity-60">${interview.jobPosition}</p>
-                        <p class="jobDetails opacity-60">${interview.jobDetails}</p>
-                    </div>
-
-
-                    <div>
-                        <p class="status bg-blue-100 opacity-80 inline px-[14px] py-3 font-semibold text-black">${interview.status}</p>
-                    </div>
-                    <div>
-                        <p class="jobDescription opacity-80 ">${interview.jobDescription}</p>
-                    </div>
-
-                    <div class="flex flex-col sm:flex-row gap-6">
-                        <button
-                            class="interview-btn px-5 py-2 text-green-500 border-[2px] border-green-500 text-lg font-semibold rounded-md">INTERVIEW</button>
-                        <button
-                            class="rejected-btn px-6 py-2 text-red-500 border-[2px] border-red-500 text-lg font-semibold rounded-md">REJECTED</button>
-                    </div>
-                </div>
-
-                <div class="card-right text-center">
-                    <button onclick="deleteApplication('delete-btn')"
-                        class="border-2 border-gray-200 p-[10px] px-[11px] rounded-full text-[13px]" id="delete-btn"><i
-                            class="fa-solid fa-trash"></i></button>
-                </div>
-        `
-        filteredSection.appendChild(div);
-    }
-    checkEmpty(filteredSection);
-
-}
-
-
-
-function renderRejected() {
-    filteredSection.innerHTML = ''
-
-    for (let rejected of rejectedCount) {
-
-        let div = document.createElement('div')
-        div.className = 'card flex justify-between p-8 shadow-md bg-white border border-gray-200 rounded-lg'
-        div.innerHTML = `
-                        <div class="card left space-y-6">
-
-                    <div class="left-top space-y-4">
-                        <p class="workPosition text-2xl font-bold opacity-80">${rejected.workPosition}</p>
-                        <p class="jobPosition opacity-60">${rejected.jobPosition}</p>
-                        <p class="jobDetails opacity-60">${rejected.jobDetails}</p>
-                    </div>
-
-
-                    <div>
-                        <p class="status bg-blue-100 opacity-80 inline px-[14px] py-3 font-semibold text-black">${rejected.status}</p>
-                    </div>
-                    <div>
-                        <p class="jobDescription opacity-80 ">${rejected.jobDescription}</p>
-                    </div>
-
-                    <div class="flex flex-col sm:flex-row gap-6">
-                        <button
-                            class="interview-btn px-5 py-2 text-green-500 border-[2px] border-green-500 text-lg font-semibold rounded-md">INTERVIEW</button>
-                        <button
-                            class="rejected-btn px-6 py-2 text-red-500 border-[2px] border-red-500 text-lg font-semibold rounded-md">REJECTED</button>
-                    </div>
-                </div>
-
-                <div class="card-right text-center">
-                    <button onclick="deleteApplication('delete-btn')"
-                        class="border-2 border-gray-200 p-[10px] px-[11px] rounded-full text-[13px]" id="delete-btn"><i
-                            class="fa-solid fa-trash"></i></button>
-                </div>
-        `
-        filteredSection.appendChild(div);
-    }
-
-    checkEmpty(filteredSection);
-}
-
-
-function deleteApplication(id) {
-    const card = document.getElementById(id);
-    
-    card.remove();
-
-    const cards = document.getElementById('allCard')
-    console.log(cards);
-
-    total.innerText = cards.children.length;
-
-    const jobCount = document.getElementById('jobs-count')
-    jobCount.innerText = cards.children.length;
-
-
-    checkEmpty(allCardSection);
-
-
-}
-
-function updateAllSectionStatus(workPosition, newStatus) {
-
-    const cards = document.getElementById('allCard').children;
-
-    for (let i = 0; i < cards.length; i++) {
-        const title = cards[i].querySelector('.workPosition').innerText;
-
-        if (title === workPosition) {
-            cards[i].querySelector('.status').innerText = newStatus;
+    for (const t of tabs) {
+        const tabName = document.getElementById("tab-" + t);
+        if (t === tab) {
+            tabName.classList.remove(...tabInactive);
+            tabName.classList.add(...tabActive);
+        } else {
+            tabName.classList.add(...tabInactive);
         }
     }
 
 
-}
+    const sections = [allContainer, interviewContainer, rejectedContainer];
 
-
-function checkEmpty(section) {
-    const empty = document.getElementById('empty');
-
-    if (section.children.length === 0) {
-        empty.classList.remove('hidden');
-    } else {
-        empty.classList.add('hidden');
+    for (const section  of sections) {
+        section.classList.add('hidden');
+    }
+    if(tab === 'all'){
+        allContainer.classList.remove('hidden');
+    }else  if(tab === 'interview'){
+        interviewContainer.classList.remove('hidden');
+    }else{
+        rejectedContainer.classList.remove('hidden');
     }
 }
+changeTab(currentTab);
